5295cc2830df5afbf1b6fe977c17a33429079ebf
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-03-07 04:18:22 +0600

    Added copied to clipboard

--- src/components/GeneratedPassword.tsx
+++ src/components/GeneratedPassword.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import usePasswordStore from "../store";
+
+
 
 
 function GeneratedPassword() {
@@ -17,7 +20,17 @@ function GeneratedPassword() {
     generatePassword
   } = usePasswordStore();
 
+  const [copied, setCopied]=useState();
+
   const handleGeneratePassword=()=>generatePassword();
+
+  const copyToClipboard=async()=>{
+    if(!generatedPassword) return;
+    await navigator.clipboard.writeText(generatedPassword);
+    setCopied(true);
+    setTimeout(()=>setCopied(false),1000)
+  };
+
   return (
     <div className="lg:p-12 p-10 lg:w-[500px] lg:h-[600px] w-3/4 h-1/2 lg:mt-16 mt-8 border-1 mx-auto bg-gray-50 shadow-2xl rounded-lg">
       <h1 className="text-2xl font-bold my-4 mb-10 text-center">Password Generator</h1>
@@ -45,9 +58,13 @@ function GeneratedPassword() {
         </div>
         <button onClick={handleGeneratePassword} className="mt-2 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">Generate Password</button>
       {generatedPassword && (
-        <div className="mt-4 p-4 rounded-lg border border-gray-200 bg-gray-100">
-          <p className="text-balance break-all">{generatedPassword}</p>
+        <div className="mt-4 p-4 rounded-lg border border-gray-200 bg-gray-100 flex justify-between items-center ">
+          <span className="text-balance break-all">{generatedPassword}</span>  
+          <button onClick={copyToClipboard} className="hover:cursor-pointer ">
+            {copied? "Copied ✓" : "Copy"}
+          </button>
         </div>
+        
       )}
 
       </div>
